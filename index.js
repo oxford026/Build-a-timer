fc55dc30d7f3ac2eb7c95daebc1156c99b2d5b14
oxford026 <153386275+oxford026@users.noreply.github.com>
2025-01-28 14:44:56 +0300

    update

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
+const circle = document.querySelector('circle');
 
+const parimeter = circle.getAttribute('r') * 2 * Math.PI;
+circle.setAttribute('stroke-dasharray', parimeter);
+
+let currentOffset = 0;
 const timer = new Timer(durationInput, startButton, pauseButton , {
     onStart() {
         console.log('Timer started');
     },
 
     onTick() {
-        console.log('Timer just ticked down');
+        circle.setAttribute('stroke-dashoffset', currentOffset);
+        currentOffset = currentOffset - 50;
     },
 
     onPausue() { 
